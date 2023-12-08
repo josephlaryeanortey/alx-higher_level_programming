5dd6fe6ef0f277f5768d0ed9ce4c6b94a8afeb63
josephlaryeanortey <christiangospelhype@gmail.com>
2023-12-07 16:54:04 -0800

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,10 @@
+2-replace_in_list.py
+
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
-    elif idx >= len(my_list):
-        return None
-    return my_list[idx]
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
